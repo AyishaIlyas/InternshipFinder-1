b668b628d710e561bc075af3ca787bafe409f854
Ayisha Ilyas <ayishailyas21@gmail.com>
2024-02-21 12:52:27 +0530

    updated version

--- InternFind/intern/views.py
+++ InternFind/intern/views.py
@@ -1,3 +1,4 @@
+from audioop import reverse
 from django.forms import ValidationError
 from django.shortcuts import render, redirect
 from . models import *
@@ -96,6 +97,7 @@ def all_applicants(request):
     return render(request, "all_applicants.html", {'application':application})
 
 def signup(request):
+    next_param = request.GET.get('next', '/')
     if request.method=="POST":   
         username = request.POST['username']
         first_name=request.POST['first_name']
@@ -121,12 +123,13 @@ def signup(request):
         applicants = Applicant.objects.create(user=user, phone=phone, gender=gender, image=image, type="applicant")
         user.save()
         applicants.save()
-        return render(request, "user_login.html")
-    return render(request, "signup.html")
+        return redirect(f"/user_login/?next={next_param}")
+    return render(request, "signup.html", {'next_param': next_param})
 
     
 
 def company_signup(request):
+    next_param = request.GET.get('next', '/')
     if request.method == "POST":
         try:
             email = request.POST['email']
